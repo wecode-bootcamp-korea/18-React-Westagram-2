c73d303ca6164651af61153a5bb14ff2a759566b
권유진 <yoojin-kwon@gwon-yujin-ui-MacBookPro.local>
2021-03-08 20:29:46 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/Pages/yoojinkwon/Login/Login.js
+++ src/Pages/yoojinkwon/Login/Login.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import './Login.scss';
 
-class Login extends Component {
+class LoginYoojin extends Component {
+    constructor(){
+        super();
+        this.state = {
+            id : " ",
+            pw : " ",
+        };
+    }
+
+    goToMain = () => {
+        this.props.history.push('/main');
+    }  
+
+    handleIdInput = (e) => {
+        this.setState({
+            id : e.target.value,
+        })
+    }
+
+    handlePwInput = (e) => {
+        this.setState({
+            pw : e.target.value,
+        })
+    }
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <div className="Login">
+                <div className="box">
+                        <h1>Westagram</h1>
+                        <input onChange={this.handleIdInput} className="inputBox" type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
+                        <input onChange={this.handlePwInput} className="inputBox" type="password" placeholder="비밀번호" />   
+                        <button className="loginButton" onClick={this.goToMain}>로그인</button>
+                    
+                        <div className="breakLine">
+                            <div className="line"></div>
+                            <span>또는</span>
+                            <div className="line"></div>
+                        </div>
+
+                        <div className="fb">
+                            <i className="fab fa-facebook-square"></i>
+                            <a className="fbLogin" href="https://www.facebook.com/login">
+                            &nbsp;Facebook으로 로그인
+                            </a>
+                        </div>
+                        <a className="forgetPassword" href="https://www.instagram.com/accounts/password/reset/">
+                        비밀번호를 잊으셨나요?
+                        </a>
+                </div>
+            </div>    
         );
     }
 }
 
-export default Login;
+export default withRouter(LoginYoojin);
